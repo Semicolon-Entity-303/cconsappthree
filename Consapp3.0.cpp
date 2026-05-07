43d254221450e9544f5da6605611a4cba559e544
semicolon <164753083+Semicolon-Entity-303@users.noreply.github.com>
2026-05-07 16:31:07 +0200

    LC-?0012
    First time implementing user-defined types; still got some issues so this version doesn't contain a new build.

--- Consapp3.0.cpp
+++ Consapp3.0.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64eddea6e92fca9a316296ab9bd5101b30d5858068c0d1802c5d85cb73ea103a
-size 7250
+oid sha256:9a6d449b6dbf6b518db45955a310a3e93db4318edf1932ea39bba98035032ae3
+size 7583
